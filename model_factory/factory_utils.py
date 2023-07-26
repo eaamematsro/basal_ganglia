3c1afa5568386b8ba1c8cb789c0c0632bbf40636
eaamematsro <elom.amematsro@columbia.edu>
2023-07-26 06:48:29 -0400

    implemented abstract rnn class

---
+++ model_factory/factory_utils.py
@@ -0,0 +1,11 @@
+import numpy as np
+import torch
+from typing import Optional
+
+
+def torchify(input_array: np.ndarray, device: Optional[torch.device] = None) -> torch.Tensor:
+    array = torch.from_numpy(input_array).to(torch.float)
+    if device is not None:
+        array.to(device)
+
+    return array
