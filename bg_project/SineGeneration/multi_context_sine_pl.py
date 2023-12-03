a8b60549abcb5232ee4cf9c33d5126e53f866a04
eaamematsro <elom.amematsro@columbia.edu>
2023-12-03 17:58:12 -0500

    modified sine training

--- bg_project/SineGeneration/multi_context_sine_pl.py
+++ bg_project/SineGeneration/multi_context_sine_pl.py
@@ -23,22 +23,27 @@ def split_dataset(dataset, fractions: Sequence = (0.6, 0.2, 0.2)):
 
 
 if __name__ == "__main__":
+    duration = 300
     torch.set_float32_matmul_precision("medium")
-    simple_model = GenerateSinePL(network="VanillaRNN")
+    simple_model = GenerateSinePL(network="VanillaRNN", duration=duration)
 
-    dataset = SineDataset()
+    dataset = SineDataset(duration=duration)
+    batch_size = 64
 
     train_set, val_set, test_set = split_dataset(dataset, (0.6, 0.2, 0.2))
 
     train_loader = DataLoader(
-        train_set["data"], batch_size=24, sampler=train_set["sampler"], num_workers=10
+        train_set["data"],
+        batch_size=batch_size,
+        sampler=train_set["sampler"],
+        num_workers=10,
     )
-    val_loader = DataLoader(val_set["data"], batch_size=24, num_workers=10)
+    val_loader = DataLoader(val_set["data"], batch_size=batch_size, num_workers=10)
 
     save_path = set_results_path(type(simple_model).__name__)[0]
 
     trainer = Trainer(
-        max_epochs=200,
+        max_epochs=500,
         gradient_clip_val=1,
         accelerator="gpu",
         devices=1,
@@ -48,37 +53,45 @@ if __name__ == "__main__":
         model=simple_model, train_dataloaders=train_loader, val_dataloaders=val_loader
     )
 
-    trainer.test(simple_model, dataloaders=DataLoader(test_set, num_workers=10))
+    # trainer.test(simple_model, dataloaders=DataLoader(test_set, num_workers=10))
 
-    simple_model.save_model()
+    # simple_model.save_model()
+    for batch_idx, batch in enumerate(val_loader):
+        simple_model.evaluate_training(batch)
+    pdb.set_trace()
 
-    for nbg in [50]:
-        thalamic_model = GenerateSinePL(network="RNNStaticBG", nbg=nbg)
+    for nbg in [10]:
+        thalamic_model = GenerateSinePL(
+            network="GMM", nbg=nbg, n_context=2, duration=duration
+        )
 
         # Transfer and freeze weights from trained network's rnn module
         transfer_network_weights(
             thalamic_model.network, simple_model.network, freeze=True
         )
+        thalamic_model.network.swap_grad_state()
+
+        # for param in thalamic_model.network.parameters():
+        #     print(param.requires_grad, param.shape)
+        # pdb.set_trace()
         # thalamic_model.network.rnn.reconfigure_u_v()
 
         train_set, val_set, test_set = split_dataset(
-            SineDataset(amplitude=-1), (0.6, 0.2, 0.2)
+            SineDataset(amplitudes=(0.75, 1.25), duration=duration), (0.6, 0.2, 0.2)
         )
 
         train_loader = DataLoader(
             train_set["data"],
-            batch_size=24,
+            batch_size=batch_size,
             sampler=train_set["sampler"],
             num_workers=10,
         )
-        val_loader = DataLoader(
-            val_set["data"], batch_size=24, sampler=val_set["sampler"], num_workers=10
-        )
+        val_loader = DataLoader(val_set["data"], batch_size=batch_size, num_workers=10)
 
         save_path = set_results_path(type(thalamic_model).__name__)[0]
 
         trainer = Trainer(
-            max_epochs=750,
+            max_epochs=400,
             gradient_clip_val=1,
             accelerator="gpu",
             devices=1,
@@ -92,3 +105,4 @@ if __name__ == "__main__":
 
         trainer.test(thalamic_model, dataloaders=DataLoader(test_set, num_workers=10))
         thalamic_model.save_model()
+        pdb.set_trace()
