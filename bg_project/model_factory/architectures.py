5c51e6e6ba32bc928393a84e184207d832ae23ef
eaamematsro <elom.amematsro@columbia.edu>
2023-12-05 10:00:29 -0500

    changed default std for classifier

--- bg_project/model_factory/architectures.py
+++ bg_project/model_factory/architectures.py
@@ -318,7 +318,7 @@ class RNNGMM(BaseArchitecture):
             output_size=n_classes,
             include_bias=include_bias,
             non_linearity=bg_nfn,
-            std=0.1,
+            std=1/n_classes,
         )
 
         self.bg = GaussianMixtureModel(
