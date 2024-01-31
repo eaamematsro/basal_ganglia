d8af9f96a61f9486287708b6ed063d2b3d146738
eaamematsro <elom.amematsro@columbia.edu>
2024-01-31 12:56:52 -0500

    added new plotting functions

--- bg_project/pacman/multigain_pacman.py
+++ bg_project/pacman/multigain_pacman.py
@@ -26,21 +26,28 @@ def split_dataset(dataset, fractions: Sequence = (0.6, 0.2, 0.2)):
 
 
 if __name__ == "__main__":
-    test_networks = [
-        "RNNStaticBG",
-        "VanillaRNN",
-    ]
+    learning_type = [
+        "inputs",
+        "pallidal",
+        "cortical",
+    ]  # defines which synapse are learnable and which should be frozen
+
+    n_epochs_base = 15  # number of training epochs for the base model
+    n_epochs_finetune = 10  # number of training epochs for the fine tuned model
+    nbg = 10  # number of thalamic and basal ganglia neurons
+
     network_type = "PallidalRNN"
     weight_penalties = np.logspace(-5, -1)
     weight_penalty = 0
     torch.set_float32_matmul_precision("medium")
     trial_duration = 150
 
-    n_batches = 5
+    n_batches = 5  # number of episodes to collect
 
+    # Define set of environment parameters to learn over
     polarity = [1]
     viscosities = np.linspace(0, 1, 10)
-    masses = np.linspace(0.5, 2, 10)
+    masses = np.linspace(0.5, 5, 10)
     spring_constants = np.linspace(0.5, 2, 10)
     for _ in range(n_batches):
 
@@ -50,6 +57,7 @@ if __name__ == "__main__":
             apply_energy_penalty=("r_act",),
             output_weight_penalty=weight_penalty,
             bg_input_size=3,
+            nbg=nbg,
         )
         dataset = PacmanDataset(
             n_samples=5000,
@@ -74,7 +82,7 @@ if __name__ == "__main__":
         save_path = set_results_path(type(simple_model).__name__)[0]
 
         trainer = Trainer(
-            max_epochs=5,
+            max_epochs=n_epochs_base,
             gradient_clip_val=10,
             accelerator="gpu",
             devices=1,
@@ -87,28 +95,7 @@ if __name__ == "__main__":
             val_dataloaders=val_loader,
         )
 
-        for batch_idx, batch in enumerate(val_loader):
-            loss = simple_model.evaluate_training(batch)
-        plt.close("all")
-
-        for batch_idx, batch in enumerate(val_loader):
-            simple_model.change_context(batch, new_context=(1, 0, 1, 2))
-        pdb.set_trace()
-        plt.close("all")
-
-        for batch_idx, batch in enumerate(val_loader):
-            simple_model.change_context(batch, new_context=(2, 0, 1, 0.5))
-        pdb.set_trace()
-        plt.close("all")
-
-        for batch_idx, batch in enumerate(val_loader):
-            simple_model.change_context(batch, new_context=(1, 2, 1, 0.5))
-        pdb.set_trace()
-        plt.close("all")
-
         simple_model.save_model()
-
-        pdb.set_trace()
 
         gain_vectors = []
         contexts = []
@@ -116,8 +103,10 @@ if __name__ == "__main__":
         for mass, gain, vis, spring_k in product(
             masses, polarity, viscosities, spring_constants
         ):
-            for network in test_networks:
-                print(f"Mass: {mass}, polarity: {gain}, viscosity: {vis}")
+            for learning_style in learning_type:
+                print(
+                    f"Mass: {mass}, polarity: {gain}, viscosity: {vis}, Spring Constant: {spring_k}"
+                )
                 train_set, val_set, test_set = split_dataset(
                     PacmanDataset(
                         n_samples=5000,
@@ -144,7 +133,7 @@ if __name__ == "__main__":
                 thalamic_model = MultiGainPacMan(
                     network=network_type,
                     duration=trial_duration,
-                    nbg=10,
+                    nbg=nbg,
                     output_weight_penalty=0,
                     bg_input_size=3,
                     teacher_output_penalty=weight_penalty,
@@ -154,6 +143,7 @@ if __name__ == "__main__":
                     mass=mass,
                     spring_constant=spring_k,
                     original_model=simple_model.network.save_path.root,
+                    learning_style=learning_style,
                 )
 
                 # Transfer and freeze weights from trained network's rnn module
@@ -162,11 +152,27 @@ if __name__ == "__main__":
                     simple_model.network,
                     freeze=True,
                 )
+                learnable_params = thalamic_model.network.learning_styles[
+                    learning_style
+                ]
+                thalamic_model.network.swap_grad_state(grad_state=False)
+
+                if learning_style == "inputs":
+                    thalamic_model.network.swap_grad_state(
+                        grad_state=True,
+                        params_to_swap=[
+                            thalamic_model.network.rnn.I.environment_params
+                        ],
+                    )
+                else:
+                    thalamic_model.network.swap_grad_state(
+                        grad_state=True, params_to_swap=learnable_params
+                    )
 
                 save_path = set_results_path(type(thalamic_model).__name__)[0]
 
                 trainer = Trainer(
-                    max_epochs=25,
+                    max_epochs=n_epochs_finetune,
                     gradient_clip_val=10,
                     accelerator="gpu",
                     devices=1,
@@ -184,12 +190,12 @@ if __name__ == "__main__":
                     dataloaders=DataLoader(test_set["data"], num_workers=10),
                 )
 
-                for batch_idx, batch in enumerate(val_loader):
-                    thalamic_model.evaluate_training(
-                        batch, original_network=simple_model
-                    )
-                    plt.pause(0.1)
-                    pdb.set_trace()
+                # for batch_idx, batch in enumerate(val_loader):
+                #     thalamic_model.evaluate_training(
+                #         batch, original_network=simple_model
+                #     )
+                #     plt.pause(0.1)
+                # pdb.set_trace()
 
                 # for batch_idx, batch in enumerate(val_loader):
                 #     simple_model.change_context(batch, new_context=(2, 0, -1))
---
+++ bg_project/pacman/plot_results.py
@@ -0,0 +1,74 @@
+import pdb
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from SineGeneration.analyze_models_sl import make_axis_nice, set_plt_params
+from pathlib import Path
+
+
+if __name__ == "__main__":
+    current_dir = Path(__file__).parent
+    csv_file = current_dir / "model_dataframe.csv"
+
+    results = pd.read_csv(csv_file, index_col=None)
+
+    set_plt_params()
+
+    grouped = results.groupby(
+        [
+            "n_hidden",
+            "polarity",
+            "mass",
+            "viscosity",
+            "spring_constant",
+            "learning_style",
+            "n_params",
+        ]
+    )
+    test = grouped["loss"].mean()
+    mean_pd = test.reset_index()
+    # r_pd = r_pd.rename(columns={"loss": f"{net_type}_loss"})
+
+    plt_params = {
+        "x": "n_params",
+        "y": "loss",
+        "hue": "learning_style",
+        "data": mean_pd,
+    }
+
+    g = sns.lineplot(**plt_params)
+    sns.scatterplot(**plt_params)
+    g.set(xscale="log")
+    make_axis_nice()
+    g = sns.FacetGrid(
+        row="learning_style", data=mean_pd.loc[mean_pd["spring_constant"] == 0.5]
+    )
+    g.map(sns.scatterplot, x="mass", y="viscosity", data=mean_pd.loc[mean_pd["spring_constant"] == 0.5],
+          hue='loss')
+    plt.pause(0.1)
+    learning_options = np.unique(mean_pd["learning_style"].tolist())
+    n_opts = len(learning_options)
+    fig, ax = plt.subplots(n_opts, sharex=True, sharey=True)
+    for idx, learning_style in enumerate(learning_options):
+        reduced_data = mean_pd.loc[
+            (mean_pd["learning_style"] == learning_style)
+            & (mean_pd["spring_constant"] == 0.5)
+        ][["mass", "viscosity", "spring_constant", "loss"]].values
+        ax[idx].set_title(learning_style)
+        ax[idx].hexbin(
+            reduced_data[:, 0],
+            reduced_data[:, 1],
+            C=reduced_data[:, -1],
+            vmin=0,
+            vmax=3,
+            xscale="log",
+        )
+        # ax[idx].set_xscale("log")
+        # ax[idx].set_yscale("log")
+    ax[-1].set_xlabel("Mass")
+    ax[-1].set_ylabel("Viscosity")
+    make_axis_nice(fig)
+    fig.tight_layout()
+    plt.pause(0.1)
+    pdb.set_trace()
