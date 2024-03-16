6d2fa4354be14eaa32bbf6df9db03977c2fa1ad3
eaamematsro <elom.amematsro@columbia.edu>
2024-03-16 12:47:27 -0400

    Add image observation space

--- bg_project/rl_games/envs/grid_worlds.py
+++ bg_project/rl_games/envs/grid_worlds.py
@@ -1,3 +1,5 @@
+import pdb
+
 import numpy as np
 from rl_games.envs.custom_env import CustomEnv
 from rl_games.pygames.rl_games import GridWorld, MultiWorldGridWorld
@@ -5,38 +7,31 @@ from gymnasium import spaces
 
 
 class GridWorldEnv(CustomEnv):
-    def _initialize_spaces(self):
+    def _initialize_spaces(self, image_obs: bool = True):
         self.action_space = spaces.Box(low=-20, high=20, shape=(2,), dtype=np.float32)
-        self.observation_space = spaces.Box(
-            low=-0.5,
-            high=0.5,
-            # high=np.array(
-            #     [
-            #         self.pygame.width,
-            #         self.pygame.width,
-            #         self.pygame.height,
-            #         self.pygame.height,
-            #     ]
-            # ),
-            shape=(4,),
-            dtype=np.float32,
-        )
-
+        if image_obs:
+            self.observation_space = spaces.Box(
+                low=0, high=255, shape=(self.pygame.width, self.pygame.height, 3)
+                , dtype=np.uint8)
+        else:
+            self.observation_space = spaces.Box(
+                low=-0.5,
+                high=0.5,
+                shape=(4,),
+                dtype=np.float32,
+            )
+            
     def __init__(self, **kwargs):
         super().__init__(pygame=GridWorld, **kwargs)
         self.pygame.reset()
 
 
 class MultiWorldGridWorldEnv(CustomEnv):
-    def _initialize_spaces(self):
-        self.action_space = spaces.Box(low=-20, high=20, shape=(2,), dtype=np.float32)
-        self.observation_space = spaces.Box(
-            low=-0.5,
-            high=0.5,
-            shape=(4,),
-            dtype=np.float32,
-        )
-
     def __init__(self, **kwargs):
         super().__init__(pygame=MultiWorldGridWorld, **kwargs)
         self.pygame.reset()
+
+
+if __name__ == '__main__':
+
+    test_world = GridWorldEnv()
